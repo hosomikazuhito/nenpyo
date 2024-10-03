ef68182ff707720e4f65dfe5dd5b7e1647511a17
hosomikazuhito <hosomi95@gmail.com>
2024-10-03 14:23:17 +0900

    年表完成とデプロイ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def to_japanese_era(year)
+    case year
+    when 1868..1911
+      "明治#{year - 1867}"
+    when 1912..1925
+      "大正#{year - 1911}"
+    when 1926..1988
+      "昭和#{year - 1925}"
+    when 1989..2018
+      "平成#{year - 1988}"
+    else
+      "令和#{year - 2018}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'years#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :years do
+    collection do
+      get 'age'
+    end
+  end
 end
